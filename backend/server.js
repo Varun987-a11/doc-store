d676c2acb6fb5d6a5d2ae1ab7295e42566e17eff
Varun <varunkumaraj12@gmail.com>
2026-03-11 18:22:06 +0530

    feature working

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+console.log("Starting server file...")
+
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req,res)=>{
+   res.send("Doc-Store backend running...")
+})
+
+app.get("/api/test", (req,res)=>{
+   res.json({
+      message: "Doc-Store API working"
+   })
+})
+
+app.listen(5500, ()=>{
+   console.log("Server running on port 5500")
+})
---
+++ frontend/doc-store/src/App.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+
+function App() {
+
+  const [message,setMessage] = useState("Loading...")
+
+  useEffect(()=>{
+    fetch("http://localhost:5000/api/test")
+      .then(res => res.json())
+      .then(data => {
+        setMessage(data.message)
+      })
+  },[])
+
+  return (
+    <div>
+      <h1>Doc-Store</h1>
+      <p>{message}</p>
+    </div>
+  )
+}
+
+export default App
